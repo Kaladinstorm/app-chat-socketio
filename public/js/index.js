724d7a712044ac92eb32e81c4bd68cf804b6a627
Juan Galdames <juan.galdames.toledo@gmail.com>
2018-06-20 21:33:57 -0400

    Adding broadcast

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+//Aca se genera el request con el server, y mantiene la conexion actic
+//Si el server se cae, sigue reintentando hasta que se vuelve a conectar
+var socket = io();
+
+//La siguiente funcion es un evento tipo connect, el cual se emite cuando el socket
+//se comunica con el server
+socket.on('connect', function() {
+    console.log('Socket is connected with server');
+})
+
+socket.on('newMessage', function(message) {
+    console.log(message);
+});
+
+//El siguiente evento se emite cuando se desconecta con el server
+socket.on('disconnect', function() {
+    console.log('Disconnect from server');
+});
+
+//Aca se creara un evento personalizado, el cual se registrara el listener
+//Sera ejecutado cuando en el server se genere el emit event
+/** 
+socket.on('newEmal', function(email){
+    console.log('New Email');
+    console.log(email);
+}); */
+
+
+/**
+//Se generara un emit event, elcual ejecutara el listener personalizado que esta en el server
+socket.emit('createEmail', {
+    from: 'perra@asd.cl',
+    text: 'mascalo'
+});
+ */
--- server/server.js
+++ server/server.js
@@ -24,11 +24,35 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    //Listener que se eecutara cuando el cliente envie el emit
+    socket.on('createMessage', function(message) {
+        console.log(message);
+
+        io.emit('newMessage', { msg: 'Paren el webeo oee' });
+    });
+
+    //Aca se crea el event emit, que ejecutara el listener creado en el lado del cliente
+    //Emit acepta un segundo parametro, el cual puede ser un objeto de cualquir tipo
+    //Lo ideal es pasar un objeto json, asi se pueden especificar varias cosas
+    //El segundo argunmento sera recibido en el callback del cliente
+    socket.emit('newEmal', {
+        from: 'juan@gmail.com',
+        text: 'chupalo',
+        createdAt: 123
+    });
+
+    //Se crea un listener personalizado que sera emitido por el cliente
+    socket.on('createEmail', function(newEmail) {
+        console.log('New Email:', newEmail);
+    })
+
     //Aca se crean los eventos correspondientes al socket creado
     //Evento de desconexion con el cliente
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
+
+    
 });
 
 
